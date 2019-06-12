31b1fa89250aeab514decd50cb043c617a3d5be4
watson090 <yo.watamura@gmail.com>
2019-06-12 15:14:36 +0900

    上げ下げの順番を修正

--- dobby.py
+++ dobby.py
@@ -13,13 +13,13 @@ import pigpio
 import TactSwitch
 import MCP23017
 
-# table = [7, 6, 5, 4, 3, 2, 1, 0, 8, 9, 10, 11, 12, 13, 14, 15]
 tablea = [7, 6, 5, 4, 3, 2, 1, 0]
 tableb = [0, 1, 2, 3, 4, 5, 6, 7]
 gpa1 = MCP23017.MCP23017(1, 0x20)
 gpb1 = MCP23017.MCP23017(1, 0x20)
 gpa2 = MCP23017.MCP23017(1, 0x21)
 gpb2 = MCP23017.MCP23017(1, 0x21)
+sleep_time = 0.03
 
 def init_mcp23017():
     gpa1.open()
@@ -33,10 +33,18 @@ def init_mcp23017():
     gpb2.output_mode(MCP23017.REG_IODIRB)
 
 
-def set_lift_callback():
-    print("set lift state")
-
 def reset_lift_callback():
+    for i in range(8):
+        gpa1.reset_bit(MCP23017.REG_OLATA, i)
+        time.sleep(sleep_time)
+        gpb1.reset_bit(MCP23017.REG_OLATB, i)
+        time.sleep(sleep_time)
+        gpa2.reset_bit(MCP23017.REG_OLATA, i)
+        time.sleep(sleep_time)
+        gpb2.reset_bit(MCP23017.REG_OLATB, i)
+        time.sleep(sleep_time)
+
+def set_lift_callback():
     global wif_file
     global log
     global current_line_number
@@ -54,25 +62,32 @@ def reset_lift_callback():
 
     for i in range(8):
         if outputLifts[i]:
-            gpa1.set_bit(MCP23017.REG_OLATA, tablea[i])
+            gpa2.set_bit(MCP23017.REG_OLATA, tablea[i])
+            time.sleep(sleep_time)
         else:
-            gpa1.reset_bit(MCP23017.REG_OLATA, tablea[i])
+            gpa2.reset_bit(MCP23017.REG_OLATA, tablea[i])
+            time.sleep(sleep_time)
     for i in range(8,16):
         if outputLifts[i]:
-            gpb1.set_bit(MCP23017.REG_OLATB, tableb[i-8])
+            gpb2.set_bit(MCP23017.REG_OLATB, tableb[i-8])
+            time.sleep(sleep_time)
         else:
-            gpb1.reset_bit(MCP23017.REG_OLATB, tableb[i-8])
+            gpb2.reset_bit(MCP23017.REG_OLATB, tableb[i-8])
+            time.sleep(sleep_time)
     for i in range(16,24):
         if outputLifts[i]:
-            gpa2.set_bit(MCP23017.REG_OLATA, tablea[i-16]) 
+            gpa1.set_bit(MCP23017.REG_OLATA, tablea[i-16]) 
+            time.sleep(sleep_time)
         else: 
-            gpa2.reset_bit(MCP23017.REG_OLATA, tablea[i-16])
+            gpa1.reset_bit(MCP23017.REG_OLATA, tablea[i-16])
+            time.sleep(sleep_time)
     for i in range(24,32):
         if outputLifts[i]:
-            gpb2.set_bit(MCP23017.REG_OLATB, tableb[i-24]) 
+            gpb1.set_bit(MCP23017.REG_OLATB, tableb[i-24]) 
+            time.sleep(sleep_time)
         else: 
-            gpb2.reset_bit(MCP23017.REG_OLATB, tableb[i-24])
-
+            gpb1.reset_bit(MCP23017.REG_OLATB, tableb[i-24])
+            time.sleep(sleep_time)
 
     print(outputLifts)
     print('\n')
@@ -94,7 +109,9 @@ if __name__ == "__main__":
     for i in range(current_line_number):
         print(i + 1, wif_file.readline())
     
-    yellow_sw = TactSwitch.TactSwitch(17, pigpio.FALLING_EDGE, reset_lift_callback, 0.1)
+    white_sw = TactSwitch.TactSwitch(17, pigpio.FALLING_EDGE, set_lift_callback  , 0.1)
+    black_sw = TactSwitch.TactSwitch(27, pigpio.FALLING_EDGE, reset_lift_callback, 0.1)
+
 
     try:
         while(True):
